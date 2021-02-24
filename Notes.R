f3040a8cfb99688238e48f340fdd3040db8dbe86
Jonathan Ryan <jonathanryan576@gmail.com>
2021-02-25 05:39:51 +0700

    Initial Commit

---
+++ Notes.R
@@ -0,0 +1,13 @@
+"""Pada saat analisa dilakukan, yaitu 2021-02-13 00:20:29 +07 belum ada data sept
+sampai des 2020 di websitnya. Jadi yasudah kita analisa dulu saja data dari jan
+sampe agst.
+
+Ini data sepertinya tidak bisa dipercaya, masa selama bulan januari cuman ada
+4 orang anak jalanan di jakpus, cuman ada 3 anak terlantar di jakpus. Impossible.
+
+Data feb tidak ada anak jalanan di jakpus, tidak ada PSK, tidak ada pemulung.
+Hebat banget jakpus nggak ada pemulung sama sekali. Pengemis cuman ada 4 orang.
+Ini tuh data yang diambil dengan cara apa?
+
+Gw udh kirim email ke sudinsosialjakpus@gmail.com menanyakan mengenai hal ini.
+Sampai notes ini di edit, belum ada jawaban dari pihak sudin sosial jakpus.
